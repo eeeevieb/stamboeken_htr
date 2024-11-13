f546ba93b3e5e59c1262da381d983e57c6c8fa37
Shoilee <sarahshoilee121@gmail.com>
2024-11-13 18:18:21 +0100

    minor

--- src/LLM/Llama.py
+++ src/LLM/Llama.py
@@ -1,6 +1,6 @@
 from groq import Groq
 
-input_path = '../../image_samples/page/NL-HaNA_2.10.50_71_0006.csv'
+input_path = '../../image_samples/page/NL-HaNA_2.10.50_71_0006.xml'
 description_path = 'information_description.txt'
 
 with open(input_path, 'r') as f:
@@ -11,14 +11,17 @@ with open(description_path, 'r') as f:
 
 client = Groq()
 completion = client.chat.completions.create(
-    model="llama-3.2-90b-vision-preview",
+    model="llama-3.1-70b-versatile",
     messages=[
         {
             "role": "user",
-            "content": "From this csv file"+ input_path +"take the column 'TextEquiv Text' and extract the information metioned in this following file"+ description_file + ". Do not give me the code, just extract these informations. Just extract the information based on the csv file. If you cannot find the answer, just say infomration do not match."
+            "content": "From xml file"+ input_path +"read the value  and extract the information metioned in this following file"+ description_file + ".\
+                  Do not give me the code, just extract these informations. \
+                  Just extract the information based on the csv file. \
+                  If you cannot find the answer, just say infomration do not match."
         }
     ],
-    temperature=.01,
+    temperature=1,
     max_tokens=1024,
     top_p=1,
     stream=True,
