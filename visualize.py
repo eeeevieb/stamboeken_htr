cde7292dc7aebdd3fb1a1c66369b17aa870fd29c
eeeevieb <embiesot@hotmail.com>
2025-03-05 11:31:25 +0100

    add legend of labels, clean up code

--- visualize.py
+++ visualize.py
@@ -35,6 +35,8 @@ def get_coords(xml_path):
 
         args: 
             xml_path: path to pageXML file
+        returns: 
+            dict of different lists of coordinates for each label
     """
 
     coords = []
@@ -49,15 +51,24 @@ def get_coords(xml_path):
             namespaces={'ns': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15'}
         )
 
-        if result is None:
-            # Log file name if no TextEquiv tag is found
-            with open(os.path.join(output_path, "image_htr_error.txt"), "a+") as error_log:
-                error_log.write(f"{file_path}\n")
-            print(f"No TextEquiv tag found in {file_path}. Logged in image_htr_error.txt.")
+        # if result is None:
+        #     # Log file name if no TextEquiv tag is found
+        #     with open(os.path.join(output_path, "image_htr_error.txt"), "a+") as error_log:
+        #         error_log.write(f"{file_path}\n")
+        #     print(f"No TextEquiv tag found in {file_path}. Logged in image_htr_error.txt.")
+
+        coords = {}
 
         for line in result:
-            # text_region_id = line.getparent().get("id")
-            # text_line_id = line.get("id")
+            # get label
+            label = line.get("custom")
+            if label:
+                label = label.split("structure", 1)
+                label = label[1] if len(label) > 1 else ""
+            else:
+                label = "no label"
+
+            # get coordinates
             text_coordinates = line.find("ns:Coords", namespaces={
                 'ns': 'http://schema.primaresearch.org/PAGE/gts/pagecontent/2013-07-15'}).get("points")
             region_coords = text_coordinates.split(" ")
@@ -67,9 +78,8 @@ def get_coords(xml_path):
                 coord_new[0] = int(coord_new[0])
                 coord_new[1] = int(coord_new[1])
                 region_coords_full.append(tuple(coord_new))
-            coords.append(region_coords_full)
-        
-        # print(coords)
+            
+            coords.setdefault(label, []).append(region_coords_full)
 
     except etree.XMLSyntaxError:
         print(f"Error parsing {file_path}. File may be malformed.")
@@ -90,7 +100,6 @@ def visualize_regions(image_path, coords, resize):
 
     # if resize is needed
     if resize:
-        print(type(resize[0]))
         resized_img_path= '/root/Thesis/resized.jpg'
         resized = image.resize((resize[0], resize[1]))
         resized.save(resized_img_path)
@@ -99,24 +108,31 @@ def visualize_regions(image_path, coords, resize):
     else:
         img = plt.imread(image_path)
 
-    polygons = []
+    # dict for colors:
+    colors = {0: "red", 1: "green", 2: "blue", 3: "yellow", 4: "orange", 5: "purple"}
+    used_labels = set()
 
-    # create the different polygons
-    for coord in coords:
-        polygon = Polygon(coord)
-        polygons.append(polygon)
-
-
-    # Plot image
+    # plot image
     fig, ax = plt.subplots()
     ax.imshow(img)
 
-    # Loop through polygons and plot them
-    for polygon in polygons:
-        x, y = polygon.exterior.xy  # Extract coordinates
-        ax.plot(x, y, color="red", linewidth=2)  # Outline
-        ax.fill(x, y, color="red", alpha=0.3)  # Fill with transparency
+    # create list of the different polygons
+    for counter, item in enumerate(coords.items()):
+        polygons = []
 
+        for coord in item[1]:
+            polygon = Polygon(coord)
+            polygons.append(polygon)
+
+        # loop through polygons and plot them
+        for polygon in polygons:
+            label = item[0] if item[0] not in used_labels else None
+            x, y = polygon.exterior.xy  # Extract coordinates
+            ax.plot(x, y, color=colors[counter], linewidth=2, label=label)  # Outline
+            ax.fill(x, y, color=colors[counter], alpha=0.3)  # Fill with transparency
+            used_labels.add(item[0])
+
+    ax.legend()
 
     plt.show()
     return
