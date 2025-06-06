75c8d0c41841c97ab32c923159fcfab086b0583f
eeeevieb <embiesot@hotmail.com>
2025-06-06 12:22:21 +0200

    try

--- test_laypa.sh
+++ test_laypa.sh
@@ -7,18 +7,8 @@
 #SBATCH -p gpu_mig
 #SBATCH --gpus=1
 
-cd laypa
-
-echo cd
-
 conda activate laypa
 
-echo env
-
 python train.py -c configs/config_stamboeken.yaml -t train -v val --num-gpus 1 --opts SOLVER.IMS_PER_BATCH 32 SOLVER.MAX_ITER 1
-
-echo train
-
-
 
 
